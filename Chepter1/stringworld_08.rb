8c640e20b2f300a8c9d67af2fbc0f2be35c6298c
Kaser Luke <qaz442200156@gmail.com>
2020-12-15 01:56:04 +0900

    ストリングの処理 String#each_char、each_byte、each_line、scanの説明を追加

---
+++ Chepter1/stringworld_08.rb
@@ -0,0 +1,34 @@
+puts '======== each_char 1 ========'
+"foo\nbar".each_char { |x| puts x}
+puts '======== each_char 2 ========'
+"foobar".each_char { |x| puts x}
+puts '======== each_char == split('') 3 ========'
+"foo\nbar".split('').each {|x| puts x}
+puts '======== each_byte ========'
+"foo\nbar".each_byte { |x| puts x}
+puts '======== each_byte with chr ========'
+"foobar".each_byte { |x| puts x.chr}
+puts '======== each_line ========'
+lines = []
+page = "There is an apple tree here.
+oh The apple has just fallen from the apple tree.
+It looks delicious.\n
+end
+"
+page.each_line { |line| puts lines << line}
+puts lines.to_s
+puts '======== each_line ~= split(\'\\n\') ========'
+puts page.split(/[\n]/).to_s
+page.split('\n').each { |x| puts x}
+
+puts '======== scan ========'
+french = "\xc3\xa7a va"
+puts '======== each_byte ========'
+french.each_byte { |c| puts c}
+puts '======== scan(/./) ========'
+french.scan(/./) { |c| puts c}
+puts '======== Set Endcoding And Do scan(/./) ========'
+# encoding: utf-8
+french.scan(/./) { |c| puts c}
+puts '======== scan(/./u) ========'
+french.scan(/./u) { |c| puts c}
